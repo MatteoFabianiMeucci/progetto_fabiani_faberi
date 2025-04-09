479620cee0eb391f5ea293855acbcc7160081599
unknown <fabiani.matteo@itismeucci.com>
2025-04-09 15:37:16 +0200

    Completata funzionalità spacchettamento

--- pages/pacchetti.php
+++ pages/pacchetti.php
@@ -13,17 +13,27 @@
         require_once("./inizializzazione_sessione.php");
         if(!$_SESSION["isLogged"])
             header("Location: http://localhost/progetto_fabiani_faberi/pages/");
+            $query = "SELECT * FROM Pacchetti"; 
     ?>
-    <div>
-        <form action="" method="post">
-        <?php $query = "SELECT * FROM Pacchetti"; ?>
-        <?php foreach ($connection->query($query) as $row):?>
-            <img src="<?=$row['Immagine']?>" alt="">
-            <br>
-            <input type="submit" value="Apri pacchetto!">
-        <?php endforeach;?>
-            
-        </form>
-    </div>
+    <?php if(!isset($_SESSION['carte_pacchetto'])): ?>
+        <div>
+            <?php foreach ($connection->query($query) as $row):?>
+                <form action="apertura_pacchetto.php" method="post">
+                    <img src="<?=$row['Immagine']?>" alt="" class = "packs">
+                    <br>
+                    <input type="submit" value="Apri pacchetto!">
+                    <input type="hidden" name="id_pacchetto" value = "<?=$row['Id']?>">
+                </form>
+            <?php endforeach;?>
+        </div>
+    <?php else: ?>
+        <div>
+            <?php $carte = $_SESSION['carte_pacchetto'] ?>
+            <?php for($i = 0; $i < count($carte); $i++):?>
+                <img src="<?=$carte[$i]['Immagine']?>" alt="" class = "cards">
+            <?php endfor;?>
+        </div>
+        <?php unset($_SESSION['carte_pacchetto']); ?>
+    <?php endif; ?>
 </body>
 </html>
